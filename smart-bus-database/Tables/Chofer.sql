ed2d0be00987eb036d9d11adf75ea883b3206733
nespindola <nespindola@lightercapital.com>
2022-05-25 02:44:40 -0300

    agrego proyecto de base de datos y algunos scripts de ejemplo

---
+++ smart-bus-database/Tables/Chofer.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Chofer]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Edad] INT NULL 
+)
---
+++ smart-bus-database/Tables/Recorrido.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Recorrido]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Id_Chofer] INT NULL, 
+    CONSTRAINT [FK_Recorrido_Chofer] FOREIGN KEY ([Id_Chofer]) REFERENCES [Chofer]([Id])
+)
