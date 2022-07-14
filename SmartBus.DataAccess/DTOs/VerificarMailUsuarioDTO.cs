8a535c47f8834207a761664aa6465b5c47596910
nespindola <nespindola@lightercapital.com>
2022-07-14 17:58:12 -0300

    agrego un endpoint nuevo para verificar si el mail solicitado ya existe registrado como un usuario en el sistema y si ya fue activado o no

---
+++ SmartBus.DataAccess/DTOs/VerificarMailUsuarioDTO.cs
@@ -0,0 +1,16 @@
+﻿using SmartBus.Entities.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBus.DataAccess.DTOs
+{
+    public class VerificarMailUsuarioDTO
+    {
+        public string Email { get; set; }
+        public bool FueActivado { get; set; }
+        public TipoDeUsuario TipoDeUsuario { get; set; }
+    }
+}
