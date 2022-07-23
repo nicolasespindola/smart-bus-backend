da40e12ed472e0c1b3dd857deb7015f8169d2875
Mauro Nahuel Casciati <31457612+maurocasciati@users.noreply.github.com>
2022-07-23 18:36:26 -0300

    Agrego endpoint para completar registro (#28)

---
+++ SmartBus.DataAccess/Command/ModificarUsuarioCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SmartBus.Entities;
+using SmartBus.Entities.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBus.DataAccess.Command
+{
+    public class ModificarUsuarioCommand : IRequest<Usuario>
+    {
+        public string Nombre { get; set; }
+        public string Apellido{ get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+        public TipoDeUsuario TipoDeUsuario { get; set; }
+    }
+
+    
+}
---
+++ SmartBus.DataAccess/Handlers/ModificarUsuarioCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NHibernate;
+using SmartBus.DataAccess.Command;
+using SmartBus.DataAccess.Context;
+using SmartBus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BCryptNet = BCrypt.Net.BCrypt;
+
+namespace SmartBus.DataAccess.Handlers
+{
+    public class ModificarUsuarioCommandHandler : CommandHandler<ModificarUsuarioCommand, Usuario>
+    {
+        public ModificarUsuarioCommandHandler(ISession _session)
+            : base(_session)
+        {
+        }
+
+        public override Task<Usuario> ResolverCommand(ModificarUsuarioCommand command, CancellationToken cancellationToken)
+        {
+            var usuario = session.Query<Usuario>().FirstOrDefault(u => u.Email == command.Email && !u.Eliminado);
+
+            if (usuario == null)
+            {
+                throw new Exception("El Email '" + command.Email + "' no está invitado a la aplicación.");
+            }
+
+            usuario.Nombre = command.Nombre;
+            usuario.Apellido = command.Apellido;
+            usuario.TipoDeUsuario = command.TipoDeUsuario;
+            usuario.Contraseña = BCryptNet.HashPassword(command.Contraseña);
+
+            return Task.FromResult(usuario);
+        }
+    }
+}
